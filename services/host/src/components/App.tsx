48dce3b9e7332ab548c35f1f4eace52b50d94bcd
nalben <baroqueworks64502@gmail.com>
2025-08-12 14:38:26 +0300

    aga

---
+++ services/host/src/components/App.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {shopRoutes} from '@packages/shared/src/routes/shop'
+import {adminRoutes} from '@packages/shared/src/routes/admin'
+
+export const App =() => {
+    return (
+        <div>
+            <h1>PAGE</h1>
+            <Link to={adminRoutes.about}>ABOUT</Link>
+            <br />
+            <Link to={shopRoutes.main}>SHOP</Link>
+            <br />
+            <Outlet />
+        </div>
+    );
+};
